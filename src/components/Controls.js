f62473508a477706a6a2897c186f076e96cfe982
kronos2k4 <kronos2k4@gmail.com>
2017-03-27 10:59:03 +0300

    Убрал комментарии

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,6 +1,3 @@
-/**
- * Created by Alex on 18.03.2017.
- */
 import React from 'react';
 
 const StoryBoard = ({ index, setScreen }) => (
@@ -10,7 +7,7 @@ const StoryBoard = ({ index, setScreen }) => (
       onClick={() => { setScreen(index) }}
     >
       <div className="b-controls_icon b-controls_icon--left">
-        <svg version="1.1" id="Слой_1" x="0px" y="0px"
+        <svg version="1.1" x="0px" y="0px"
           viewBox="0 0 50 50" >
         <path fill="#808080" d="M26.6,45.9c-11.1,0-20.2-9-20.2-20.2c0-3.9,1.1-7.7,3.3-11L4.9,10l18.2-4.9l-4.9,18.2l-3.9-3.9
           c-1,1.9-1.5,4.1-1.5,6.3c0,7.6,6.1,13.7,13.7,13.7c7.6,0,13.7-6.1,13.7-13.7s-6.1-13.7-13.7-13.7h-1.5V5.6h1.5
@@ -26,7 +23,7 @@ const StoryBoard = ({ index, setScreen }) => (
         Skip
       </div>
       <div className="b-controls_icon b-controls_icon--right">
-        <svg version="1.1" id="Слой_2" x="0px" y="0px" viewBox="0 0 50 50">
+        <svg version="1.1" x="0px" y="0px" viewBox="0 0 50 50">
           <path fill="#808080" d="M40.5,29.4c0-0.7-0.4-0.9-1-0.4L24,42.2c-0.3,0.2-0.7,0.2-0.9-0.1l-2.5-3c-0.2-0.3-0.2-0.7,0.1-0.9l13.8-11.8
             c0.3-0.2,0.3-0.6,0-0.8L20.7,13.8c-0.3-0.2-0.3-0.6-0.1-0.9l2.5-3c0.2-0.3,0.6-0.3,0.9-0.1l15.5,13.2c0.5,0.5,1,0.3,1-0.4v-6.2
             c0-0.3,0.3-0.6,0.6-0.6h4.4c0.3,0,0.6,0.3,0.6,0.6v19.2c0,0.4-0.3,0.6-0.6,0.6h-4.4c-0.3,0-0.6-0.3-0.6-0.6V29.4z M25.4,25.2
